9525b507649f2390c698879c1a764a410c6a8898
Artem <mexexll96@gmail.com>
2020-10-06 15:46:46 +0300

    Updates

---
+++ precache-manifest.8c8fc44db648c8b96f854e8634472125.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb8ccd07ca52174ab0bb4b6db415183",
+    "url": "/react-state-and-lifecycle/index.html"
+  },
+  {
+    "revision": "477c5802d7ad82ae667c",
+    "url": "/react-state-and-lifecycle/static/js/2.36d44999.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-state-and-lifecycle/static/js/2.36d44999.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37b62b7addec06191640",
+    "url": "/react-state-and-lifecycle/static/js/main.1c42faf0.chunk.js"
+  },
+  {
+    "revision": "bfe8ae8ae5d16d4a7744",
+    "url": "/react-state-and-lifecycle/static/js/runtime-main.ae7b9754.js"
+  }
+]);
